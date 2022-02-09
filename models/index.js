ed7ffa1ecc4a46b05c2602bdec254cb33b273c1a
Ose Ughu <zinjmyster10@gmail.com>
2022-02-09 22:37:24 +0100

    add config.json

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const path = require('path')
 const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
-const config = require(`${__dirname}/../config/config.js`)[env]
+const config = require(`${__dirname}/../config/config.json`)[env]
 const db = {}
 
 let sequelize
@@ -41,6 +41,5 @@ Object.keys(db).forEach(modelName => {
 })
 
 db.sequelize = sequelize
-db.Sequelize = Sequelize
 
 module.exports = db
