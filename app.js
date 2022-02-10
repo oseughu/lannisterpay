45f6536e3504ecec0adcf2246f44c8592d232393
Ose Ughu <zinjmyster10@gmail.com>
2022-02-10 08:47:40 +0100

    add code 500 to config error

--- app.js
+++ app.js
@@ -190,7 +190,7 @@ app.post(
         feeConfig = 'LNPY1225'
         appliedFee = flatPerc(20, 0.5)
       } else {
-        return res.json({
+        return res.status(500).json({
           error: 'No valid configuration exists for this payment entity.'
         })
       }
