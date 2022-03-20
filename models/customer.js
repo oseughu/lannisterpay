74abb1a2fee4f0a374aa3531935757e852fc055b
Ose Ughu <zinjmyster10@gmail.com>
2022-03-20 19:47:34 +0100

    refactoring

--- models/customer.js
+++ models/customer.js
@@ -1,5 +1,7 @@
 'use strict'
+
 const { Model } = require('sequelize')
+
 module.exports = (sequelize, DataTypes) => {
   class Customer extends Model {
     static associate({ Transaction, PaymentEntity }) {
--- models/paymentEntity.js
+++ models/paymentEntity.js
@@ -1,5 +1,7 @@
 'use strict'
+
 const { Model } = require('sequelize')
+
 module.exports = (sequelize, DataTypes) => {
   class PaymentEntity extends Model {
     static associate({ Customer }) {
