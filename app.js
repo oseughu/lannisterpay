ec664df542c2aa0d2df7f848be28cefaa580c628
Ose Ughu <zinjmyster10@gmail.com>
2022-04-06 09:25:00 +0100

    add redis caching

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ import 'dotenv/config'
 import _ from 'lodash'
 import compression from 'compression'
 import express, { json } from 'express'
-import Redis from 'ioredis'
+import mongoose from 'mongoose'
+import mongooseRedisCaching from 'mongoose-redis-caching'
 import { Fee, Transaction } from './models/schema.js'
 
-const redis = new Redis(`${process.env.REDIS_URL}`)
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(compression())
 app.use(json())
+mongooseRedisCaching(mongoose)
 
 app.post(
   '/fees',
@@ -28,43 +29,53 @@ app.post(
       feePerc, //optional, percentage value to be charged for the transaction if feeType is PERC or FLAT PERC
       feeValue
 
-    let newArr = FeeConfigurationSpec.split('\n')
-
-    newArr.forEach(spec => {
-      let specArr = spec.split(' ')
-      let feeArr = specArr[7].split(':')
-      feeId = specArr[0]
-      feeCurrency = specArr[1]
-      feeLocale = specArr[2]
-      feeEntity = specArr[3]
-      entityProperty = specArr[4]
-      feeType = specArr[6]
-      feeFlat = +parseFloat(feeArr[0]).toFixed(2) || 0
-      feePerc = +parseFloat(feeArr[1]).toFixed(2) || 0
-      feeValue =
-        feeType === 'FLAT_PERC'
-          ? `${feeFlat}:${feePerc}`
-          : feeType === 'FLAT'
-          ? feeFlat
-          : feePerc
-    })
-
     try {
-      const fee = new Fee({
-        FeeId: feeId,
-        FeeLocale: feeLocale,
-        FeeCurrency: feeCurrency,
-        FeeEntity: feeEntity,
-        EntityProperty: entityProperty,
-        FeeType: feeType,
-        FeeFlat: feeFlat,
-        FeePerc: feePerc,
-        FeeValue: feeValue
+      let newArr = FeeConfigurationSpec.split('\n')
+
+      newArr.forEach(spec => {
+        let specArr = spec.split(' ')
+        let entityArr = specArr[3].split('(')
+        let feeArr = specArr[7].split(':')
+        feeId = specArr[0]
+        feeCurrency = specArr[1]
+        feeLocale = specArr[2]
+        feeEntity = entityArr[0]
+        entityProperty = entityArr[1].slice(0, -1)
+        feeType = specArr[6]
+        feeFlat =
+          feeType === 'FLAT'
+            ? specArr[7]
+            : feeType === 'FLAT_PERC'
+            ? +parseFloat(feeArr[0]).toFixed(2)
+            : 0
+        feePerc =
+          feeType === 'PERC'
+            ? specArr[7]
+            : feeType === 'FLAT_PERC'
+            ? +parseFloat(feeArr[1]).toFixed(2)
+            : 0
+        feeValue =
+          feeType === 'FLAT_PERC'
+            ? `${feeFlat}:${feePerc}`
+            : feeType === 'FLAT'
+            ? feeFlat
+            : feePerc
+
+        Fee.create({
+          FeeId: feeId,
+          FeeLocale: feeLocale,
+          FeeCurrency: feeCurrency,
+          FeeEntity: feeEntity,
+          EntityProperty: entityProperty,
+          FeeType: feeType,
+          FeeFlat: feeFlat,
+          FeePerc: feePerc,
+          FeeValue: feeValue
+        })
       })
-      await fee.save()
-      res.json(fee)
+      res.json({ message: 'Fee Configs successfully added.' })
     } catch (error) {
-      //console.log(error)
+      console.log(error)
       res.status(500).json(error)
     }
   }
@@ -77,10 +88,10 @@ app.post(
     const { ID, Amount, Currency, CurrencyCountry, Customer, PaymentEntity } =
       req.body
 
-    let customer, paymentMethod, locale, type, property, value, chargeAmount
+    let customer, paymentMethod, locale, value, chargeAmount
 
     try {
-      const transaction = new Transaction({
+      const transaction = await Transaction.create({
         ID,
         Amount,
         Currency,
@@ -88,37 +99,34 @@ app.post(
         Customer,
         PaymentEntity
       })
-      await transaction.save()
 
       customer = transaction.Customer
 
       paymentMethod = transaction.PaymentEntity
 
-      paymentMethod.Country === CurrencyCountry
+      paymentMethod.Country === transaction.CurrencyCountry
         ? (locale = 'LOCL')
         : (locale = 'INTL')
 
-      type = _.toUpper(_.kebabCase(paymentMethod.Type))
-
-      property = _.upperCase(paymentMethod.Brand)
-
       const feeConfig = await Fee.findOne({
-        where: {
-          FeeCurrency: Currency,
-          FeeLocale: locale,
-          FeeEntity: type,
-          EntityProperty: property
-        }
-      })
+        FeeCurrency: Currency,
+        $or: [{ FeeLocale: locale }, { FeeLocale: '*' }],
+        $or: [{ FeeEntity: paymentMethod.Type }, { FeeEntity: '*' }],
+        $or: [
+          { EntityProperty: paymentMethod.Brand },
+          { EntityProperty: paymentMethod.Issuer },
+          { EntityProperty: '*' }
+        ]
+      }).cache(3600)
 
       !feeConfig &&
         res.status(400).json({
           error: 'No valid configuration exists for this payment method.'
         })
 
-      _.upperCase(feeConfig.FeeType) === 'FLAT'
+      feeConfig.FeeType === 'FLAT'
         ? (value = feeConfig.FeeFlat)
-        : _.upperCase(feeConfig.FeeType) === 'PERC'
+        : feeConfig.FeeType === 'PERC'
         ? (value =
             (feeConfig.FeePerc * +parseFloat(transaction.Amount).toFixed(2)) /
             100)
@@ -138,7 +146,7 @@ app.post(
         SettlementAmount: chargeAmount - value
       })
     } catch (error) {
-      //console.log(error)
+      console.log(error)
       res.status(500).json(error)
     }
   }
