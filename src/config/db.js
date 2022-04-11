a3ff6f8136ea8d6f5d468403d47392fac729324c
Ose Ughu <zinjmyster10@gmail.com>
2022-04-11 09:51:56 +0100

    add db uri to .env file

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,4 @@
 import 'dotenv/config'
 import mongoose from 'mongoose'
 
-export const connectToDb = () => mongoose.connect(`${process.env.MONGO_URL}`)
+export const connectToDb = () => mongoose.connect(process.env.MONGO_URL)
