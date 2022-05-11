c76a195c83c03a2c3d2ad371120b44671db06b65
Ose Ughu <zinjmyster10@gmail.com>
2022-05-11 14:28:53 +0100

    remove compression

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import 'dotenv/config'
 import express, { json } from 'express'
-import compression from 'compression'
 import { connectToDb } from '#config/db'
 import { routes } from '#routes'
 
@@ -8,7 +7,6 @@ const port = process.env.PORT || 3000
 const app = express()
 connectToDb()
 
-app.use(compression())
 app.use(json())
 app.use(routes)
 
